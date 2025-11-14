c3d619a0f589aef1f7664604e3bd33c2fe7eb9c2
professeurdjire <hamidoudjirehmd@gmail.com>
2025-11-14 14:24:06 +0000

    Integration en cours

--- src/main/java/com/example/edugo/controller/AuthController.java
+++ src/main/java/com/example/edugo/controller/AuthController.java
@@ -12,6 +12,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
